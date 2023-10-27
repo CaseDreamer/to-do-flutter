c19d6acc7ebab106ae53df72d55fdbf6c5c4c207
Finn-Dev <duluxpw@gmail.com>
2023-10-27 19:17:45 +1100

    submit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(255, 255, 0, 0);
+const Color tdBlue = Color(0xFFFFA600);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color.fromARGB(255, 122, 119, 119);
+
+const Color tdBGColor = Color.fromARGB(255, 179, 179, 179);
